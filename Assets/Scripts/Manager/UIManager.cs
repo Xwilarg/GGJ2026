bc514e18b1abd3530fe87561ad86ca4263e4f559
Xwilarg <xwilarg@protonmail.com>
2026-01-31 10:26:04 +0100

    Add empty interaction wheel

---
+++ Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace GGJ2026.Manager
+{
+    public class UIManager : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _maskWheel;
+
+        private void Awake()
+        {
+            _maskWheel.SetActive(false);
+        }
+    }
+}
