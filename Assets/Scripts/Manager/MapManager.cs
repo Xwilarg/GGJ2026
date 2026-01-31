69055936118490bd727fe04ff92910df21fd6696
Xwilarg <xwilarg@protonmail.com>
2026-01-31 19:53:58 +0100

    Improve map gen

--- Assets/Scripts/Manager/MapManager.cs
+++ Assets/Scripts/Manager/MapManager.cs
@@ -25,27 +25,42 @@ namespace GGJ2026.Manager
             Room startingRoom = null;
 
             Dictionary<int, Room> tmpRooms = new();
+
+            void AddRoom(int depth, int x)
+            {
+                var xPos = x * RoomSize;
+                var yPos = (depth - x - 1) * RoomSize;
+                var pos = new Vector2Int(xPos, yPos);
+                var room = GenerateRoom(xPos, yPos);
+
+                if (startingRoom == null)
+                {
+                    startingRoom = room;
+                }
+                else
+                {
+                    if (tmpRooms.TryGetValue(new Vector2Int(xPos - RoomSize, yPos).GetHashCode(), out var xRoom)) xRoom.LeftDoor = new Door() { LinkedRoom = room };
+                    if (tmpRooms.TryGetValue(new Vector2Int(xPos, yPos - RoomSize).GetHashCode(), out var yRoom)) yRoom.RightDoor = new Door() { LinkedRoom = room };
+                }
+
+                tmpRooms.Add(pos.GetHashCode(), room);
+                _rooms.Add(room);
+            }
+
             for (int depth = 1; depth <= _genInfo.GenerationDepth; depth++)
             {
                 for (int x = 0; x < depth; x++)
                 {
-                    var xPos = x * RoomSize;
-                    var yPos = (depth - x - 1) * RoomSize;
-                    var pos = new Vector2Int(xPos, yPos);
-                    var room = GenerateRoom(xPos, yPos);
-
-                    if (startingRoom == null)
-                    {
-                        startingRoom = room;
-                    }
-                    else
-                    {
-                        if (tmpRooms.TryGetValue(new Vector2Int(xPos - RoomSize, yPos).GetHashCode(), out var xRoom)) xRoom.LeftDoor = new Door() { LinkedRoom = room };
-                        if (tmpRooms.TryGetValue(new Vector2Int(xPos, yPos - RoomSize).GetHashCode(), out var yRoom)) yRoom.RightDoor = new Door() { LinkedRoom = room };
-                    }
-
-                    tmpRooms.Add(pos.GetHashCode(), room);
-                    _rooms.Add(room);
+                    AddRoom(depth, x);
+                }
+            }
+            int tmp = _genInfo.GenerationDepth;
+            int off = 0;
+            for (int depth = _genInfo.GenerationDepth + 1; depth <= _genInfo.GenerationDepth * 2; depth++/*, tmp--*/, off++)
+            {
+                for (int x = off; x < tmp; x++)
+                {
+                    AddRoom(depth, x);
                 }
             }
 
