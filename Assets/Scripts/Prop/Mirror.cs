c15bbf0c0b44a1df36b572c75822ed852e17f3d5
Xwilarg <xwilarg@protonmail.com>
2026-01-31 17:36:30 +0100

    Update mirror

--- Assets/Scripts/Prop/Mirror.cs
+++ Assets/Scripts/Prop/Mirror.cs
@@ -1,5 +1,6 @@
 ﻿using GGJ2026.Manager;
 using GGJ2026.Player;
+using System.Linq;
 using UnityEngine;
 
 namespace GGJ2026.Prop
@@ -7,6 +8,28 @@ namespace GGJ2026.Prop
     public class Mirror : MonoBehaviour, IInteractible
     {
         public string AssociatedLine { set; get; }
+        private MaskType _mask;
+        private Collider _coll;
+
+        public void SetAssociatedMask(MaskType mask)
+        {
+            _mask = mask;
+            UpdateCollisions(MaskManager.Instance.CurrentMask);
+        }
+
+        private void Awake()
+        {
+            _coll = GetComponents<Collider>().First(x => !x.isTrigger);
+            MaskManager.Instance.OnMaskChange.AddListener((mask) =>
+            {
+                UpdateCollisions(mask);
+            });
+        }
+
+        private void UpdateCollisions(MaskType currMask)
+        {
+            _coll.enabled = _mask == currMask;
+        }
 
         EntityId IInteractible.Key => gameObject.GetEntityId();
 
