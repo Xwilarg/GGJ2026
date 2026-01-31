a5204e390663eb4d73b73493d65e87522cb844bf
Xwilarg <xwilarg@protonmail.com>
2026-01-31 20:30:52 +0100

    Add main menu

---
+++ Assets/Scripts/Manager/MenuManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GGJ2026.Manager
+{
+    public class MenuManager : MonoBehaviour
+    {
+        public void PlayGame()
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
